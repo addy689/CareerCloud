9f5c4ed83f0e8243cbfc763dc1596a133f1d5de9
Addy Singh <addy689@gmail.com>
2017-06-08 22:27:58 -0400

    Assignment1
    Created all DB table pocos

---
+++ CareerCloud.Pocos/CompanyJobEducationPoco.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CareerCloud.Pocos
+{
+    [Table("Company_Job_Educations")]
+    public class CompanyJobEducationPoco : IPoco
+    {
+        [Key] 
+        [Column("Id")]
+        public Guid Id { get; set; }
+
+        [Column("Job")]
+        public Guid Job { get; set; }
+
+        [Column("Major")] 
+        public string Major { get; set; }
+
+        [Column("Importance")] 
+        public short Importance { get; set; }
+
+        [Column("Time_Stamp")] 
+        public byte[] TimeStamp { get; set; }
+    }
+}
